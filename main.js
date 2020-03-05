5a8aba271f0f59ec67829ac19bf011ce00369df9
mateki0 <mateki0@interia.pl>
2020-03-05 17:22:42 +0100

    navbar updated

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-var language = document.querySelector('.language');
-var languageDropdown = document.querySelector('.language-dropdown');
+// Navbar dropdowns !
 
-function openDropdown() {
+const language = document.querySelector('.language');
+const languageDropdown = document.querySelector('.language-dropdown');
+
+const account = document.querySelector('.account');
+const accountDropdown = document.querySelector('.account-dropdown');
+
+function openAccountDropdown() {
+  accountDropdown.style.display = 'flex';
+  document.body.classList.add('overlay');
+}
+
+function closeAccountDropdown() {
+  accountDropdown.style.display = 'none';
+  document.body.classList.remove('overlay');
+}
+
+
+function openLanguageDropdown() {
   languageDropdown.style.display = "flex";
   document.body.classList.add('overlay');
 }
 
-function closeDropdown(el) {
+function closeLanguageDropdown(el) {
   languageDropdown.style.display = "none";
   document.body.classList.remove('overlay');
 }
-language.addEventListener('mouseover', openDropdown);
-language.addEventListener('mouseleave', closeDropdown);
-languageDropdown.addEventListener('mouseover', openDropdown)
-languageDropdown.addEventListener('mouseleave', closeDropdown)
+language.addEventListener('mouseover', openLanguageDropdown);
+language.addEventListener('mouseleave', closeLanguageDropdown);
+languageDropdown.addEventListener('mouseover', openLanguageDropdown);
+languageDropdown.addEventListener('mouseleave', closeLanguageDropdown);
+
+account.addEventListener('mouseover', openAccountDropdown);
+account.addEventListener('mouseleave', closeAccountDropdown);
+accountDropdown.addEventListener('mouseover', openAccountDropdown);
+accountDropdown.addEventListener('mouseleave', closeAccountDropdown);
+
+//
